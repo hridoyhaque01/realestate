f2a17304e85c1a2898f38478804c809a78aeb5ed
Hridoy <iamnirob334@gmail.com>
2023-02-13 23:04:29 +0600

    fixed some issue

--- js/main.js
+++ js/main.js
@@ -1,3 +1,55 @@
+// header start
+
+const header = document.querySelector("header");
+const headerHeight = header.offsetHeight;
+const menuItems = document.querySelectorAll(".main-menu .nav-link");
+
+function scrollWindow() {
+  const scrollTop = window.scrollY;
+
+  if (scrollTop > 0) {
+    header.classList.add("minimize");
+  } else {
+    header.classList.remove("minimize");
+  }
+}
+
+const navbarActive = () => {
+  let position = window.scrollY + headerHeight;
+  menuItems.forEach((link) => {
+    if (!link.hash) return;
+    const section = document.querySelector(link.hash);
+    if (!section) return;
+    if (
+      position >= section.offsetTop &&
+      position <= section.offsetTop + section.offsetHeight
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const targetId = link.getAttribute("href");
+      const targetSection = document.querySelector(targetId);
+      const targetPosition = targetSection.offsetTop - headerHeight;
+
+      window.scrollTo({
+        top: targetPosition,
+        behavior: "smooth",
+      });
+    });
+  });
+};
+
+window.addEventListener("load", navbarActive);
+window.addEventListener("scroll", navbarActive);
+
+window.addEventListener("load", scrollWindow);
+window.addEventListener("scroll", scrollWindow);
+
 const container1 = document.querySelector(".filter-contents");
 const mixer1 = mixitup(container1, {
   selectors: {
